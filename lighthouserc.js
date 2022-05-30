65db404d0dc84aa5f08808c1bef813f25ffe40d7
Troy <86826396+tsheed@users.noreply.github.com>
2022-05-30 16:31:56 +0100

    WIP

--- lighthouserc.js
+++ lighthouserc.js
@@ -9,10 +9,6 @@ module.exports = {
     },
     assert: {
       assertions: {
-        // "categories:performance": ["error", { minScore: 0.95 }],
-        // "categories:accessibility": ["error", { minScore: 0.95 }],
-        // "categories:best-practices": ["error", { minScore: 0.95 }],
-        // "categories:seo": ["error", { minScore: 0.95 }],
         "categories:performance": ["error"],
         "categories:accessibility": ["error"],
         "categories:best-practices": ["error"],
